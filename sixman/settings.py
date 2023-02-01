7ba6b97fce96f61760214aec264b3ac49db54f3b
Sodole <lws0915@gmail.com>
2023-02-01 16:14:39 +0900

    test

--- sixman/settings.py
+++ sixman/settings.py
@@ -135,4 +135,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['']
+CSRF_TRUSTED_ORIGINS = ['https://port-0-omoproject-1jx7m2gldlbuls5.gksl2.cloudtype.app/']
